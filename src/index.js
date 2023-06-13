56906c7430012741214788d789befc9947afc3c8
Jeongpyo <jp.941009@gmail.com>
2023-06-13 19:13:17 +0900

    class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-// import Counter from "./Counter";
+// import App from "./App";
+import Counter from "./Counter";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <App />
-    {/* <Counter /> */}
+    {/* <App /> */}
+    <Counter />
   </div>
 );
 
